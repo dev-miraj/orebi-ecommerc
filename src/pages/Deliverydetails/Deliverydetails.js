d23f9f9482e6216106a053817c0551dfd3e57763
Md Mirajul Islam <mdm144532@gmail.com>
2024-04-15 22:29:23 +0530

    orebi

---
+++ src/pages/Deliverydetails/Deliverydetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Deliverydetails = () => {
+  return (
+    <div className='flex justify-center items-center'>
+     <h1>Comming Soon</h1>
+    </div>
+  )
+}
+
+export default Deliverydetails
